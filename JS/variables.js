7c40ccf8492737a1c23686729677c80eb0ad9771
SEGUNDORAMSES <cano1soto@gmail.com>
2024-07-02 20:05:34 -0600

    geirel

---
+++ JS/variables.js
@@ -0,0 +1,13 @@
+console.log("hola mundo")
+
+
+
+let nombre="Geirel"
+let apellido="Cano"
+let edad="28"
+let direccion="Guadalupe"
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(direccion)
