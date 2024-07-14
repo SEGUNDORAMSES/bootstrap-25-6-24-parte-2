6033d0231d7a921ab7ee66512d36413231fa4056
SEGUNDORAMSES <cano1soto@gmail.com>
2024-07-14 17:37:17 -0600

    geirel

--- JS/calculadora.js
+++ JS/calculadora.js
@@ -1,1 +1,36 @@
-console.log("working")
+//console.log("working")
+
+function Calculadora2(event) {
+  event.preventDefault();
+
+  let number1 = parseFloat(document.getElementById("Num1").value);
+  let number2 = parseFloat(document.getElementById("Num2").value);
+  let operaciones = document.getElementById("operaciones").value;
+  let resultado = document.getElementById("Resultado");
+
+  let valor;
+  switch (operaciones) {
+    case "-":
+      valor = number1 - number2;
+      break;
+
+    case "*":
+      valor = number1 * number2;
+      break;
+
+    case "+":
+      valor = number1 + number2;
+      break;
+
+    case "/":
+      valor= number1 / number2;
+      break;
+
+    default:
+      valor="la operacion es invalida"
+    
+    break;
+}
+alert("EL RESULTADO ES:  "+valor )
+
+}
--- JS/minombre.js
+++ JS/minombre.js
@@ -1,17 +1,1 @@
-// console.log("working")
-
-
-
-function guardar() {
-    const formulario = document.getElementById("formulario");
-    const valor1 = document.getElementById("valor1").value;
-    const valor2 = document.getElementById("valor2").value;
-    const valorrespuesta = document.getElementById("valorrespuesta");
-    alert("el valor es: " + valor1 +  "el valor 2 es:  " + valor2)
-    valorrespuesta.textcontent="el valor es: " + valor1 +  "el valor 2 es:  " + valor2
-
-}
-
-
-
-
+console.warn("working")
