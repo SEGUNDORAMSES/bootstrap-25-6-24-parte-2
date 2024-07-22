a90fcb5844f995459695cedde6ba7564bb83a0b0
SEGUNDORAMSES <cano1soto@gmail.com>
2024-07-21 20:18:02 -0600

    geirel

---
+++ tarea de JS.js
@@ -0,0 +1,20 @@
+
+
+    let series = [
+        { id: 1, Serie: "Ana", Categoria: "Drama", Temporadas: 2 },
+        { id: 2, Serie: "Stranger Things", Categoria: "Ciencia Ficción", Temporadas: 4 },
+        { id: 3, Serie: "The Crown", Categoria: "Historia", Temporadas: 5 },
+        { id: 4, Serie: "La Casa de Papel", Categoria: "Crimen", Temporadas: 5 }
+    ];
+
+
+    series.forEach(series => {
+    alert(series.Serie)
+
+    })
+
+
+
+
+
+
