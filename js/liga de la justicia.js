85fba6f927198159931ba3fe4762d338ba7cc71b
SEGUNDORAMSES <cano1soto@gmail.com>
2024-07-31 21:03:33 -0600

    entregable proyecto 2 img

--- js/liga de la justicia.js
+++ js/liga de la justicia.js
@@ -1,10 +1,15 @@
+console.log("funcionando")
+// 1--Evento que carga la funcion cuando el DOM este en la pagina 
+//Control + Space
 document.addEventListener('DOMContentLoaded', () => {
 
-    mostrar_ligadelajusticia(heroes)
+    Mostrar_LigaDeLaJusticia(Heroes) //Carge la pagina  
 
 })
 
-const heroes = [
+
+// 2- Arreglo de objetos
+const Heroes = [
     {
         nombre: "Aquaman",
         bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
@@ -28,20 +33,43 @@ const heroes = [
         bio: "Es una superheroína ficticia creada por el psicólogo y escritor William Moulton Marston para la editorial DC Comics. Sus historias se centran en los ideales del amor, paz y sexualidad.",
         img: "img/wonder_woman.png",
         casa: "DC"
-    }
+    },
+
 ];
 
+// 3-Funcion que muestra los heroes 
+//A  -ID del contendor
+//B  -Plantilla
+//C  -Ciclo de la parte 2 (2- Arreglo de objetos) 
+//D Meter la Plantilla en el contenedor 
 
+function Mostrar_LigaDeLaJusticia(Heroes) { //Firma
+    // let Contenedor //A
+    let Contenedor = document.getElementById("contenedorHeroes")
+    // let Plantilla  //B
+    let plantilla = ""
+    // //C 
+    Heroes.forEach(element => {
+        plantilla += `
+            <div class=" col-lg-4 mb-4">
+            <div class="card h-100">
+                <img class="card-img-top" src="${element.img}" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">${element.nombre}</h4>
+                    <p class="card-text">${element.bio}</p>
+                    <button type="button" name="" id="" class="btn btn-primary" btn-lg btn-block">Ver mas </button>
 
+                </div>
+            </div>
+        </div>
+        
+    `
+    });
 
-function mostrar_ligadelajusticia(heroes) {
-         let contenedor = document.getElementById("contenedoresheroes")
-    let plantilla =
-    console.log(JSON.stringify(heroes))
-     
-
-    
-
-
-    // contenedor.innerHTML=plantilla
+    // Contenedor.innerHtml = Plantilla //D
+    Contenedor.innerHTML = plantilla
+    console.log(Heroes)
 }
+
+//JSON
+//String 
--- js/navbar.js
+++ js/navbar.js
@@ -1,86 +1,73 @@
-//Componente NavBar
 document.addEventListener('DOMContentLoaded', () => {
+
     Mostrar_NavBar();
+    Datos_LI(ListadoMenu);
 
 })
-// Objetos de JS
-//Let Contenedor =ID
-//Let Plantilla = "Hola Mundo objetos de JS"
-//Contenedor.innerHTML = Plantilla
+
+
+const ListadoMenu = [
+    {
+        Nombre: "Inicio",
+        Ruta: "Index.html"
+    },
+    {
+        Nombre: "Listado de Productos",
+        Ruta: "List-Products.html"
+    },
+    {
+        Nombre: "Crear Producto",
+        Ruta: "New-Product.html"
+    },
+    {
+        Nombre: "Pagina de Mercadeo",
+        Ruta: ""
+    }
+]
+
 
 function Mostrar_NavBar() {
-    // let Opcion1 = "Inicio";
-    // let Opcion2 = "Listado de Productos";
-    // let Opcion3 = "Crear Producto";
-    // let Opcion4 = "Pagina de Mercadeo";
-    // let Menu={
-    //     Nombre: "Inicio",
-    //     Ruta:"Index.html"
-    // }
-    //Arreglo [{},{},{}]
 
-    //Bucles o Ciclos 
-    let ListadoMenu = [
-        {
-            Nombre: "Inicio",
-            Ruta: "index.html"
-        },
-        {
-            Nombre: "Listado de Productos",
-            Ruta: "list products.html"
-        },
-        {
-            Nombre: "Crear Producto",
-            Ruta: "new product.html"
-        },
-        {
-            Nombre: "Pagina de Mercadeo",
-            Ruta: ""
-        }
-    ]
     console.log(ListadoMenu)
     let Contenedor = document.getElementById('V_navbar')
 
     let Plantilla = `
-
- <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="collapsibleNavId">
-                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="${ListadoMenu[0].Ruta}">${ListadoMenu[0].Nombre} <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="${ListadoMenu[1].Ruta}">${ListadoMenu[1].Nombre}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="${ListadoMenu[2].Ruta}">${ListadoMenu[2].Nombre}</a>
-                        </li>
-                       <li class="nav-item">
-                            <a class="nav-link" href="#">${ListadoMenu[3].Nombre}</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                            <div class="dropdown-menu" aria-labelledby="dropdownId">
-                                <a class="dropdown-item" href="#">Action 1</a>
-                                <a class="dropdown-item" href="#">Action 2</a>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" onclick="CerrarSesion()">Cerrar Sesion</a>
-                        </li>
-                    </ul>
-                    <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-                    </form>
-                </div>
-
-`
+ 
+  <a class="navbar-brand" href="#">Navbar</a>
+                 <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                     aria-expanded="false" aria-label="Toggle navigation">
+                     <span class="navbar-toggler-icon"></span>
+                 </button>
+                 <div class="collapse navbar-collapse" id="collapsibleNavId">
+                     <ul class="navbar-nav mr-auto mt-2 mt-lg-0" id="Nav_LI">
+                     
+                        
+         
+                        
+                     </ul>
+                     <form class="form-inline my-2 my-lg-0">
+                         <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                     </form>
+                 </div>
+ 
+ `
 
     Contenedor.innerHTML = Plantilla
 
+
+}
+
+function Datos_LI(datos) {
+    let contenedor2 = document.getElementById("Nav_LI")
+    let plantilla = ""
+    datos.forEach(element => {
+        plantilla += `
+          <li class="nav-item active">
+             <a class="nav-link" href="${element.Ruta}">${element.Nombre} <span class="sr-only">(current)</span></a>
+          </li>
+         `
+    });
+
+    contenedor2.innerHTML = plantilla
 }
